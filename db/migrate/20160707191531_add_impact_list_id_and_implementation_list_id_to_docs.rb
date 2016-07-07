1495cd01980595e5b8f715624b4b38b656a8d140
Woody Peterson <woody.peterson@gmail.com>
2016-07-07 16:50:23 -0700

    Fix migration issue

--- db/migrate/20160707191531_add_impact_list_id_and_implementation_list_id_to_docs.rb
+++ db/migrate/20160707191531_add_impact_list_id_and_implementation_list_id_to_docs.rb
@@ -2,6 +2,5 @@ class AddImpactListIdAndImplementationListIdToDocs < ActiveRecord::Migration[5.0
   def change
     add_column :docs, :impact_list_id, :integer
     add_column :docs, :implementation_list_id, :integer
-    remove_column :doc_lists, :doc_id
   end
 end
